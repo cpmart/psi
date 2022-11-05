e6ee14c280de99ed7a43b50338b0fa3259144c79
cpmart <cpmart@gmail.com>
2022-11-05 16:07:26 -0300

    exercício-dom

---
+++ ex15-array.js
@@ -0,0 +1,11 @@
+let valores = [];
+
+valores.push('a');
+valores.push('b');
+valores.push(2);
+valores.push('a');
+valores.push('b');
+valores.push(2);
+
+
+console.log(valores);
---
+++ ex17-array.js
@@ -0,0 +1,4 @@
+let arr = ['banana', 'cajá','melão', 'mamão', 'caju'];
+
+console.log(arr[arr.length-1]);
+console.log(arr.at(0));
